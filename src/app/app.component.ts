8a8ba2f4cc791afdcca6a2e0e13446c9c8dde1af
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-25 14:08:08 +0545

    counter app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  count = 0;
+
+  handleIncrement() {
+    this.count++;
+  }
+
+  handleDecrement() {
+    this.count--;
+  }
+
+  handleReset() {
+    this.count = 0;
+  }
 }
