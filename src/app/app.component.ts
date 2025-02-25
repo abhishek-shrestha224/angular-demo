91d50f8f4b8c8582ba74e12be2ccf23b3b4743fd
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-25 19:47:07 +0545

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+// flex fix clean red kkkllllggh
 export class AppComponent {
   count = 0;
 
