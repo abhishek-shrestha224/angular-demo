ddbc20e75429ac50ce052e1db73d1821b97e2755
Abhishek Shrestha <meabhi224@gmail.com>
2025-02-25 21:26:27 +0545

    ...

---
+++ src/app/input-value/input-value.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-value',
+  imports: [],
+  templateUrl: './input-value.component.html',
+  styleUrl: './input-value.component.css'
+})
+export class InputValueComponent {
+
+}
